eb22bcc7fefd17a84a5b43a9d8e9609f419cc453
AngelMelendres <fabian_melendres@hotmail.com>
2024-01-19 10:57:23 -0500

    proceso responsive

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,8 @@ import Main from "./Pages/Main";
 import Places from "./Pages/Places";
 import Activities from "./Pages/Activities";
 import Contact from "./Pages/Contact";
+import Services from "./Pages/Services";
+import Blog from "./Pages/Blog";
 
 function App() {
   return (
@@ -12,11 +14,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/places" element={<Places />}></Route>
-          <Route path="/activities" element={<Activities/>}></Route>
-          <Route path="/services" element={<Main />}></Route>
-          <Route path="/services" element={<Main />}></Route>
-          <Route path="/contact" element={<Contact/>}></Route>
-          <Route path="/blog" element={<Main />}></Route>
+          <Route path="/activities" element={<Activities />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/blog" element={<Blog />}></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ vite-project/src/Pages/Blog.jsx
@@ -0,0 +1,217 @@
+import React from "react";
+const Blog = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-10">
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="shadow rounded flex justify-between overflow-auto items-center">
+          <img
+            className="h-32"
+            src="https://img.freepik.com/foto-gratis/paisaje-forestal_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1705622400&semt=ais"
+            alt="Image 1"
+          />
+          <div>
+            <h1 className="text-center font-bold">Titulo im</h1>
+            <p className=" p-2 mx-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            </p>
+            <div className="flex justify-around text-red-500">
+              <p>❤</p>
+              <p>100%</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
